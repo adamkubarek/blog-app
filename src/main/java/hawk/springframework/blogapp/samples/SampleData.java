76ac27ee8461e89e673696ed065d4d81bac22a8f
AdamPC <adamko9400@gmail.com>
2017-10-31 00:21:53 +0100

    Preparing Real Sample Data & Minor CSS fixes

--- src/main/java/hawk/springframework/blogapp/samples/SampleData.java
+++ src/main/java/hawk/springframework/blogapp/samples/SampleData.java
@@ -43,26 +43,41 @@ public class SampleData implements ApplicationListener <ContextRefreshedEvent> {
 			throw new RuntimeException("Java tag nie istnieje");
 		}
 	
-		Optional <Tag> sportTagOptional = tagRepository.findByName("Spring");
-		if(!sportTagOptional.isPresent()) {
-			throw new RuntimeException("Sport tag nie istnieje");
+		Optional <Tag> springTagOptional = tagRepository.findByName("Spring");
+		if(!springTagOptional.isPresent()) {
+			throw new RuntimeException("Spring tag nie istnieje");
+		}
+		
+		Optional <Tag> algorithmTagOptional = tagRepository.findByName("Algorytmy");
+		if(!algorithmTagOptional.isPresent()) {
+			throw new RuntimeException("Algorytmy tag nie istnieje");
 		}
 		
 		Tag javaTag = javaTagOptional.get();
-		Tag sportTag = sportTagOptional.get();
+		Tag springTag = springTagOptional.get();
+		Tag algorithmTag = algorithmTagOptional.get();
 		
 		Article firstArticle = new Article();
 		firstArticle.setAuthor("admin");
-		firstArticle.setShortDescription("This is short description of a first Article");
-		firstArticle.setContent("This is short description of a first Article This is short description of a first Article This is short description of a first Article This is short description of a first Article");
-		firstArticle.setTitle("First article");
+
+		firstArticle.setContent("Jednym ze sposobów rozwiązywania złożonych problemów jest zastosowanie algorytmów równoległych."
+				+ " Oznacza to, że program nie jest wykonywany tylko jeden raz na jednym procesorze, ale wielokrotnie równolegle"
+				+ " na wielu różnych maszynach. Podejście takie jest stosowane od lat w superkomputerach, jednak w takiej realizacji"
+				+ " jest ono bardzo kosztowne. Nowym pomysłem jest tutaj zastosowanie sieci zwykłych komputerów tworzących klaster."
+				+ " Całe zadanie jest wtedy rozdzielane na wiele maszyn i obliczane równolegle przy pomocy tysięcy procesorów. Czasami"
+				+ " taką potężną sieć rozproszoną nazywa się z j. angielskiego grid. Przykładem jej zastosowania może być program SETI@home,"
+				+ " gdzie dane z nasłuchu kosmosu analizują dziesiątki tysięcy komputerów należących do zwykłych użytkowników. Maszyny"
+				+ " są podłączone do Internetu, przez który przesyłają wyniki pracy uruchomionych na nich aplikacji. Rozwinięciem tego rozwiązania"
+				+ " jest projekt parasolowy BOINC@home, który obejmuje kilkadziesiąt tego typu projektów co SETI@home, zajmujących się"
+				+ " zagadnieniami z wielu dziedzin nauki, nie tylko ścisłych.Obecnie algorytmy równoległe mogą być wykorzystywane na zwykłych domowych"
+				+ " komputerach, ponieważ ogromna większość z nich posiada procesory wielordzeniowe, które w uproszczeniu są połączeniem kilku procesorów w jeden.");
+		firstArticle.setShortDescription(firstArticle.getContent().substring(0,200)+"...");
+		firstArticle.setTitle("Sposoby rozwiązywania problemów");
 		firstArticle.setTime(CurrentTime.get());
 		Set <Tag> tags = new HashSet<>();
-		tags.add(sportTag);
-		tags.add(javaTag);
+		tags.add(algorithmTag);
 		firstArticle.setTags(tags);
 
-		
 		articleRepository.save(firstArticle);
 		
 		Article secondArticle = new Article();
@@ -73,66 +88,35 @@ public class SampleData implements ApplicationListener <ContextRefreshedEvent> {
 				+ " rockets in 2024. In that same year, two flights of 100 passengers each will go to Mars to build a city using"
 				+ " the cargo which will have been delivered.");
 		secondArticle.setShortDescription(secondArticle.getContent().substring(0,200)+"...");
-
 		secondArticle.setTitle("Finding Water on Other Planets");
 		secondArticle.setTime(CurrentTime.get());
-		secondArticle.setTags(tags);
-
+		Set <Tag> tags2 = new HashSet<>();
+		tags2.add(springTag);
+		secondArticle.setTags(tags2);
 
 		articleRepository.save(secondArticle);
+		
 		
 		Article thirdArticle = new Article();
 		thirdArticle.setAuthor("admin");
 		
-		thirdArticle.setContent("This is short description of a first Article This is short description of a first Article This is"
-				+ " short description of a first Article This is short description of a first Article");
-		thirdArticle.setShortDescription("This is short description of a first Article");
-		thirdArticle.setTitle("Third article");
+		thirdArticle.setContent("Bacon ipsum dolor amet burgdoggen turducken meatball ground round spare ribs fatback brisket. "
+				+ "Shankle turkey beef ribs biltong, capicola pastrami sausage frankfurter turducken jowl pork loin filet mignon"
+				+ " brisket meatloaf meatball. Sausage prosciutto ball tip hamburger short loin. Capicola beef shoulder turducken"
+				+ " ball tip bresaola picanha pig alcatra. Hamburger shank filet mignon burgdoggen. Capicola prosciutto cow, beef ribs"
+				+ " shoulder fatback filet mignon meatloaf bacon tongue. Landjaeger t-bone pork chop strip steak tri-tip. Swine porchetta meatball tail."
+				+ " Sirloin bacon pig ribeye corned beef swine. Shank cow filet mignon short ribs alcatra. Pancetta cow corned beef jerky, cupim burgdoggen turducken "
+				+ "beef prosciutto strip steak doner. Jerky short loin biltong porchetta strip steak swine. Shankle swine tenderloin boudin, tri-tip pig andouille "
+				+ "brisket. Andouille ham kevin, turducken ribeye pork chop short ribs kielbasa leberkas cupim. Leberkas ribeye pork, spare ribs shoulder alcatra filet mignon.");
+		thirdArticle.setShortDescription(thirdArticle.getContent().substring(0,200)+"...");
+		thirdArticle.setTitle("Becon ipsum");
 		thirdArticle.setTime(CurrentTime.get());
-		thirdArticle.setTags(tags);
+		Set <Tag> tags3 = new HashSet<>();
+		tags3.add(javaTag);
+		tags3.add(algorithmTag);
+		thirdArticle.setTags(tags3);
 		articleRepository.save(thirdArticle);
 		
-		Article fourthArticle = new Article();
-		fourthArticle.setAuthor("admin");
-		fourthArticle.setContent("This is short description of a first Article This is short description of a first Article This is"
-				+ " short description of a first Article This is short description of a first Article");
-		fourthArticle.setShortDescription("This is short description of a first Article");
-		fourthArticle.setTitle("Fourth article");
-		fourthArticle.setTime(CurrentTime.get());
-		fourthArticle.setTags(tags);
-		articleRepository.save(fourthArticle);
-		
-		Article fifthArticle = new Article();
-		fifthArticle.setAuthor("admin");
-		fifthArticle.setContent("This is short description of a first Article This is short description of a first Article This is"
-				+ " short description of a first Article This is short description of a first Article");
-		fifthArticle.setShortDescription("This is short description of a first Article");
-		fifthArticle.setTitle("Fifth article");
-		fifthArticle.setTime(CurrentTime.get());
-		fifthArticle.setTags(tags);
-		articleRepository.save(fifthArticle);
-		
-		Article sixthArticle = new Article();
-		sixthArticle.setAuthor("admin");
-		sixthArticle.setContent("This is short description of a first Article This is short description of a first Article This is"
-				+ " short description of a first Article This is short description of a first Article");
-		sixthArticle.setShortDescription("This is short description of a first Article");
-		sixthArticle.setTitle("Sixth article");
-		sixthArticle.setTime(CurrentTime.get());
-		sixthArticle.setTags(tags);
-		articleRepository.save(sixthArticle);
-		
-		
-		Article seventhArticle = new Article();
-		seventhArticle.setAuthor("admin");
-		
-		seventhArticle.setContent("This is short description of a first Article This is short description of a first Article This is"
-				+ " short description of a first Article This is short description of a first Article");
-		seventhArticle.setShortDescription("This is short description of a first Article");
-		seventhArticle.setTitle("Seventh article");
-		seventhArticle.setTime(CurrentTime.get());
-		seventhArticle.setTags(tags);
-		articleRepository.save(seventhArticle);
 		
 		Comment firstComment = new Comment();
 		Comment secondComment = new Comment();
